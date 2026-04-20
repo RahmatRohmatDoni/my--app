b70fe964819866dbab31a36052ef178d2385b1ba
Rahmat RohmatDoni <rahmatrohmatdoni@gmail.com>
2026-04-20 13:54:48 +0800

    Commit Ke dua

---
+++ app/learn/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+export default function LearnPage(){
+    return(
+        <div>
+            <div>
+                <img src="/palette.svg" alt="Design" />
+                <p>Design</p>
+            </div>
+            <div>
+                <p>Figma</p>
+                <p>8 Hours</p>
+            </div>
+        </div>
+    );
+};
