9c8dbbe0a1f8f02f91742039bc77f161b401ce97
Rahmat RohmatDoni <rahmatrohmatdoni@gmail.com>
2026-05-03 08:27:56 +0800

    Belajar Tailwind CSS

--- app/task-2/page.tsx
+++ app/task-2/page.tsx
@@ -1,43 +1,78 @@
-export default function task2Page(){
+export default function BelajarPage(){
     return(
-<section className="h-screen bg-gray-100s flex gap-6">
-        <div className ="bg-[#e3f6fb] p-8 h-72 w-120 rounded-4xl flex flex-col justify-between text-blue-950" >
-            <div className = "flex gap-4 " >
-                <img src="palette.svg" alt="palette"/>
-                <p className="font-bold">Design</p>
-            </div>
-            <div className="relative">
-                <h2 className="text-4xl font bold mb-2" >Adobe Photoshop</h2>
-                <p className="font-bold">in 3 days</p>
-                    <div className="absolute right-0 bottom-0 translate-y-2 flex items-center">
-                        <img src="/jusufkalla.svg" className="w-8 h-8 rounded-full border-2 border-white" />
-                        <img src="/prabowo.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
-                        <img src="/ferdysambo.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
-                        <img src="/tambahan1.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+        <section className="h-screen bg-white flex flex-col justify-center items-center gap-4">
+            <div className="flex gap-4">
+                <div className="bg-[#e3f6fb] p-6 w-96 h-60 rounded-4xl flex flex-col justify-between">
+                    <div className="flex gap-3">
+                        <img src="palette.svg" alt="pallete" />
+                        <p className="font-sans text-black">Design</p>
                     </div>
-            </div>
-        </div>
-
-        <div className ="bg-[#e9f9e6] p-8 h-72 w-120 rounded-4xl flex flex-col justify-between text-blue-950" >
-            <div className="flex justify-between items-center">
-                <div className = "flex gap-4 " >
-                    <img src="terminal-2.svg" alt="terminal"/>
-                    <p className="font-bold">AI</p>
+                    <div className="flex justify-between items-end">
+                        <div>
+                            <p className="font-sans font-bold text-2xl text-black mb-1">Adobe Photoshop</p>
+                            <p className="font-sans text-black">in 3 days</p>
+                        </div>
+                        <div className="flex items-center">
+                            <img src="jusufkalla.svg" className="w-8 h-8 rounded-full border-2 border-white" />
+                            <img src="prabowo.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                            <img src="ferdysambo.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                            <img src="tambahan1.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                        </div>
+                    </div>
                 </div>
-                <img src="/star.svg" alt="star"/>
-            </div>
-            <div className="relative">
-                <h2 className="text-4xl font bold mb-2" >DALL•E 2, Midjourney,</h2>
-                <h2 className="text-4xl font bold mb-2" >Stable Diffusion</h2>
-                <p className="font-bold">in 5 days</p>
-                    <div className="absolute right-0 bottom-0 translate-y-2 flex items-center">
-                        <img src="/gibran.svg" className="w-8 h-8 rounded-full border-2 border-white" />
-                        <img src="/jokowi.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
-                        <img src="/pigai.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
-                        <img src="/tambahan2.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                <div className="bg-[#e9f9e6] p-6 w-96 h-60 rounded-4xl flex flex-col justify-between">
+                    <div className="flex gap-3">
+                        <img src="terminal-2.svg" alt="terminal" />
+                        <p className="font-sans text-black">AI</p>
+                        <img src="star.svg" alt="star" className="ml-auto"/>
                     </div>
+                    <div className="flex justify-between items-end">
+                        <div>
+                        <p className="font-sans font-bold text-2xl text-black">Dall·E 2, Midjourney,</p>
+                        <p className="font-sans font-bold text-2xl text-black mb-1">Stable Difusion</p>
+                        <p className="font-sans text-black">in 5 days</p>
+                        </div>
+                        <div className="flex items-center">
+                            <img src="gibran.svg" className="w-8 h-8 rounded-full border-2 border-white" />
+                            <img src="jokowi.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                            <img src="pigai.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                            <img src="tambahan2.svg" className="w-8 h-8 rounded-full border-2 border-white -ml-3" />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-</section>
+            <div className="flex gap-4">
+                <div className="bg-[#e9e5fb] p-6 rounded-4xl w-63 h-55 flex flex-col justify-between">
+                    <div className="flex gap-3">
+                        <img src="palette.svg" alt="palette" />
+                        <p className="font-sans text-black">Design</p>
+                    </div>
+                    <div>
+                        <p className="font-sans font-bold text-2xl text-black mb-1">Figma</p>
+                        <p className="font-sans text-black">8 hours ago</p>
+                    </div>
+                </div>
+                <div className="bg-[#fde8e1] p-6 rounded-4xl w-63 h-55 flex flex-col justify-between">
+                    <div     className="flex gap-3">
+                        <img src="code.svg" alt="code" />
+                        <p className="font-sans text-black">Coding</p>
+                    </div>
+                    <div>
+                        <p className="font-sans font-bold text-2xl text-black mb-1">Python</p>
+                        <p className="font-sans text-black">2 days ago</p>
+                    </div>
+                </div>
+                <div className="bg-[#fde3e9] p-6 rounded-4xl w-63 h-55 flex flex-col justify-between">
+                    <div className="flex gap-3">
+                        <img src="palette.svg" alt="palette" />
+                        <p className="font-sans text-black">Design</p>
+                    </div>
+                    <div>
+                        <p className="font-sans font-bold text-xl text-black mb-1   ">Sketch</p>
+                        <p className="font-sans text-black">4 Days ago</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
